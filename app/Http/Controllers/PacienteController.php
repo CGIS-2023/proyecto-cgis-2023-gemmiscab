7548b78982369b92019ba12377e145099c26a59d
gemmiscab <gemmiscab@alum.us.es>
2023-03-23 23:44:23 +0100

    Entrega 2.2 - Resto de archivos

---
+++ app/Http/Controllers/PacienteController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paciente;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->authorizeResource(Paciente::class, 'paciente');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pacientes = Paciente::paginate(25);
+        return view('/pacientes/index', ['pacientes' => $pacientes]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pacientes/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+            'nuhsa' => 'required|string',
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $paciente = new Paciente($request->all());
+        $paciente->user_id = $user->id;
+        $paciente->save();
+        session()->flash('success', 'Paciente creado correctamente');
+        return redirect()->route('pacientes.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('pacientes/show', ['paciente' => $paciente]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('pacientes/edit', ['paciente' => $paciente]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'nuhsa' => 'required|string',
+        ]);
+        $user = $paciente->user;
+        $user->fill($request->all());
+        $user->save();
+        $paciente->fill($request->all());
+        $paciente->save();
+        session()->flash('success', 'Paciente modificado correctamente.');
+        return redirect()->route('pacientes.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if($paciente->delete()) {
+            session()->flash('success', 'Paciente borrado correctamente.');
+        }
+        else{
+            session()->flash('warning', 'El paciente no pudo borrarse. Es probable que se deba a que tenga asociada información como recetas que dependen de él.');
+        }
+        return redirect()->route('paciente.index');
+    }
+}
