c5fbd00d30ba9ab55881ea388a91f420507baa4f
gemmiscab <gemmiscab@alum.us.es>
2023-05-03 10:11:32 +0200

    modificados dos archivos de config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Proyecto CGIS 22/23 gemmiscab'),
 
     /*
     |--------------------------------------------------------------------------
